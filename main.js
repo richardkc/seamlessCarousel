2a98b9d9d4cad025af23886196b08f095d6bd972
Richard <15917198755@163.com>
2019-08-01 21:19:15 +0800

    无缝轮播与切换轮播结合

--- main.js
+++ main.js
@@ -1,4 +1,146 @@
-let n 
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+makeFakeSlides()
+$slides.css({ transform: 'translateX(-300px)' })
+bindEvents()
+$(next).on('click', function () {
+    goToSlide(current + 1)
+})
+$(previous).on('click', function () {
+    goToSlide(current - 1)
+})
+let timer = setInterval(() => {
+    goToSlide(current + 1)
+}, 3000);
+$('.container').on('mouseenter', function () {
+    window.clearInterval(timer)
+}).on('mouseleave', function () {
+    setInterval(() => {
+        goToSlide(current + 1)
+    }, 3000);
+})
+document.addEventListener('visibilitychange',function(e){
+    if(document.hidden){
+        window.clearInterval(timer)
+    } else {
+        setInterval(() => {
+            goToSlide(current + 1)
+        }, 3000);
+    }
+})
+
+
+
+
+function bindEvents() {
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
+}
+
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length - 1
+    }
+    if (current === $buttons.length - 1 && index === 0) {  //最后一张到第一张
+        console.log('1')
+        $slides.css({ transform: `translateX(${-($buttons.length + 1) * 300}px)` })
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({ transform: `translate(${-(index + 1) * 300}px)` }).show()
+            })
+    } else if (current === 0 && index === $buttons.length - 1) {  //第一张到最后一张
+        console.log('2')
+        $slides.css({ transform: 'translateX(0)' })
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` }).show()
+            })
+    } else {
+        console.log('3')
+        $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` })
+    }
+    current = index
+}
+
+
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
+
+
+
+
+
+
+/*
+    $buttons.eq(0).on('click',function(){
+        if(current === 2){    //最后一张到第一张
+            $slides.css({transform: 'translateX(-1200px)'})
+            .one('transitionend',function(){
+                $slides.hide().offset()
+                $slides.css({transform: 'translateX(-300px)'}).show()
+            })
+        }else{
+            $slides.css({transform: 'translateX(-300px)'})
+        }
+        current = 0
+    })
+    $buttons.eq(1).on('click',function(){
+        $slides.css({transform: 'translateX(-600px)'})
+        current = 1
+    })
+    $buttons.eq(2).on('click',function(){
+        if(current=== 0){    //第一张到最后一张
+            $slides.css({transform: 'translateX(0)'})
+            .one('transitionend',function(){
+                $slides.hide().offset()
+                $slides.css({transform: 'translateX(-900px)'}).show()
+            })
+        }else{
+            $slides.css({transform: 'translateX(-900px)'})
+        }
+        current = 2
+    })
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let n 
 initialization()
 
 setInterval(() => {
@@ -40,7 +182,7 @@ function makeEnter($node){
 function getImage(n){
     return $(`.images>img:nth-child(${count(n)})`)
 }
-
+*/
 
 
 
